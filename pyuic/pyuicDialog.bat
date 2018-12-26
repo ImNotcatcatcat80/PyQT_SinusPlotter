db2ef9cba4b1ed450cc2eb0d4ecb100f09242c48
David Girardello <muccavolante@hotmail.com>
2018-12-27 00:51:22 +0100

    Add files via upload

---
+++ pyuic/pyuicDialog.bat
@@ -0,0 +1,7 @@
+
+@ PATH C:\Users\david.girardello\Downloads\sw_others\WinPython-32bit-3.4.4.1\python-3.4.4\Scripts
+PATH C:\Dev\WinPython-32bit-3.4.4.1\python-3.4.4\Scripts
+
+pyuic4 sinuses_UIdialog.ui -o UIdialogSin.py
+
+pause
---
+++ pyuic/pyuicMain.bat
@@ -0,0 +1,6 @@
+
+@ PATH C:\Dev\WinPython-32bit-3.4.4.1\python-3.4.4\Scripts
+PATH C:\Users\david.girardello\Downloads\sw_others\WinPython-32bit-3.4.4.1\python-3.4.4\Scripts
+
+pyuic4 PyQT_sinuses.ui -o UIsinuses.py
+pyuic4 sinuses_UIdialog.ui -o UIdialogSin.py
